c5f45c3bfcc22bcef0d622cc06e422c8750bfd2d
Vikanda Gonzales <vikanda.gonzales@yahoo.com>
2018-12-04 22:51:43 -0800

    made changes to create shop to match up with frontend

--- db/seeds/05-staff.js
+++ db/seeds/05-staff.js
@@ -2,10 +2,10 @@ exports.seed = function(knex, Promise) {
   return knex('staff').del()
     .then(function () {
       return knex('staff').insert([
-        {id: 1, shop_id: 1, role_id: 1, first_name: 'Claudia', last_name: 'Ligidakis', email: 'claudia@test', password: '$2a$10$M7BiKs2MPI97bz5CkSdkQehB.5gvK/zegIDlzSMIyJoPPnGPm6vwW', photo: 'https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12222652/Yorkshire-Terrier-MP.jpg'},
-        {id: 2, shop_id: 1, role_id: 2, first_name: 'Vika', last_name: 'Gonzales', email: 'vika@test', password: '$2a$10$M7BiKs2MPI97bz5CkSdkQehB.5gvK/zegIDlzSMIyJoPPnGPm6vwW', photo: 'https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12222652/Yorkshire-Terrier-MP.jpg'},
-        {id: 3, shop_id: 2, role_id: 1, first_name: 'Vika', last_name: 'Gonzales', email: 'vika@test', password: '$2a$10$M7BiKs2MPI97bz5CkSdkQehB.5gvK/zegIDlzSMIyJoPPnGPm6vwW', photo: 'https://static-cdn.jtvnw.net/jtv_user_pictures/hsdogdog-profile_image-5550ade194780dfc-300x300.jpeg'},
-        {id: 4, shop_id: 2, role_id: 2, first_name: 'Claudia', last_name: 'Ligidakis', email: 'claudia@test', password: '$2a$10$M7BiKs2MPI97bz5CkSdkQehB.5gvK/zegIDlzSMIyJoPPnGPm6vwW', photo: 'https://www.healthypawspetinsurance.com/Images/V3/DogAndPuppyInsurance/Dog_CTA_Desktop_HeroImage.jpg'}
+        {id: 1, shop_id: 1, role_id: 1, first_name: 'Claudia', last_name: 'Ligidakis', email: 'claudia@test', password: '$2a$10$5yNsaFcMNnq69yzo88MN5O9GTg29K/wbwzDVz7HBQn/f7L9Rb6Lk6', photo: 'https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12222652/Yorkshire-Terrier-MP.jpg'},
+        {id: 2, shop_id: 1, role_id: 2, first_name: 'Vika', last_name: 'Gonzales', email: 'vika@test', password: '$2a$10$5yNsaFcMNnq69yzo88MN5O9GTg29K/wbwzDVz7HBQn/f7L9Rb6Lk6', photo: 'https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12222652/Yorkshire-Terrier-MP.jpg'},
+        {id: 3, shop_id: 2, role_id: 1, first_name: 'Vika', last_name: 'Gonzales', email: 'vika@test', password: '$2a$10$5yNsaFcMNnq69yzo88MN5O9GTg29K/wbwzDVz7HBQn/f7L9Rb6Lk6', photo: 'https://static-cdn.jtvnw.net/jtv_user_pictures/hsdogdog-profile_image-5550ade194780dfc-300x300.jpeg'},
+        {id: 4, shop_id: 2, role_id: 2, first_name: 'Claudia', last_name: 'Ligidakis', email: 'claudia@test', password: '$2a$10$5yNsaFcMNnq69yzo88MN5O9GTg29K/wbwzDVz7HBQn/f7L9Rb6Lk6', photo: 'https://www.healthypawspetinsurance.com/Images/V3/DogAndPuppyInsurance/Dog_CTA_Desktop_HeroImage.jpg'}
       ]);
     })
     .then(() => {
