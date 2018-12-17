b4e390f7511f6d1a321e57c0a8dd09b1e24dd057
Vikanda Gonzales <vikanda.gonzales@yahoo.com>
2018-12-16 22:40:31 -0800

    removed password from get staff function returns

--- src/models/shop/shops.js
+++ src/models/shop/shops.js
@@ -101,18 +101,22 @@ function removeShop(shop_id) {
 ////////////////////////////////////////////////////////////////////////////////
 function getOneStaff(staff_id, shop_id) {
   return (knex('staff').where({id: staff_id, shop_id: shop_id}).first())
+  .then(({password, ...data}) => data)
 }
 
 function getStaffByEmail(staff_email, shop_id) {
   return (knex('staff').where({email: staff_email, shop_id: shop_id}).first())
+  .then(({password, ...data}) => data)
 }
 
 function getStaffByRole (role_id, shop_id) {
   return (knex('staff').where({role_id: role_id, shop_id: shop_id, archived: false}))
+  .map(({password, ...data}) => data)
 }
 
 function getAllStaff(shop_id) {
   return (knex('staff').where({shop_id: shop_id}))
+  .map(({password, ...data}) => data)
 }
 
 function createStaff (body, shop_id) {
